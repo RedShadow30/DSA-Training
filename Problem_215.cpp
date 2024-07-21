520140c57f2c6ce6a6f228d47cec0f2516ef1cc3
RedShadow30 <148815559+RedShadow30@users.noreply.github.com>
2024-07-21 12:51:17 -0500

    Create Problem_215.cpp
    Added Starter Code

---
+++ Problem_215.cpp
@@ -0,0 +1,14 @@
+/* Day 1
+  Problem 215: Kth Largest Element in an Array
+  Given an integer array nums and an integer k, return the kth largest element in the array.
+  Note that it is the kth largest element in the sorted order, not the kth distinct element.
+  Can you solve it without sorting?
+*/
+
+class Solution {
+public:
+  // Use a max heap and remove head until Kth highest is the head
+  int findKthLargestElement(vector<int>& nums, int k) {
+    
+  }
+};
